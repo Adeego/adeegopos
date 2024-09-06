59e028637472d07a9e5515459a7bc39b2e1d3d8b
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2024-09-06 07:45:39 +0300

    added top customer component

--- components/reportComps/customerReport.js
+++ components/reportComps/customerReport.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import TopCustomers from './topCustomers'
 
 export default function CustomerReport() {
   return (
-    <div>CustomerReport</div>
+    <div>
+      <TopCustomers />
+    </div>
   )
 }
